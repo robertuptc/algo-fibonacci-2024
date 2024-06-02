848dacf3c8256d4def1c2c567a704c8d792fc9d3
robertuptc <robertuptc@gmail.com>
2024-06-02 19:04:17 -0400

    problem finished

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,16 @@
 const fibonacci = (num) => {
-    
+    let fibArr = [0, 1]
+
+    if (num == 0) {
+        return fibArr[0];
+    } else if (num == 1) {
+        return fibArr[1];
+    } else {
+        for (let i = 0; i < num -1 ; i++) {
+            fibArr.push(fibArr[i] + fibArr[i + 1])
+        }
+    }
+    return fibArr[fibArr.length - 1]
 }
 
 module.exports = {fibonacci}
